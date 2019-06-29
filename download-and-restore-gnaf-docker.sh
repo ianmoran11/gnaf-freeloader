f9906e6f08d7b9014b7e98a3cddf3db562042236
Ian <ian.moran11@gmail.com>
2019-06-29 18:26:18 +1000

    Change name of docker volume to gnaf_data

--- download-and-restore-gnaf-docker.sh
+++ download-and-restore-gnaf-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo docker volume create pg_data
+sudo docker volume create gnaf_data
 
-sudo docker run --name=gnaf_postgis -p 5433:5432 -v pg_data:/var/lib/postgresql  kartoza/postgis:9.6-2.4
+sudo docker run --name=gnaf_postgis -p 5433:5432 -v gnaf_data:/var/lib/postgresql  kartoza/postgis:9.6-2.4
 
 sudo docker exec gnaf_postgis /bin/sh -c 'touch .pgpass'
 sudo docker exec gnaf_postgis /bin/sh -c 'echo "localhost:5432:gis:docker:docker" >> .pgpass'
@@ -12,14 +12,11 @@ sudo docker exec gnaf_postgis /bin/sh -c 'touch .pgpassgeo'
 sudo docker exec gnaf_postgis /bin/sh -c 'echo "localhost:5432:geo:docker:docker" >> .pgpassgeo'
 sudo docker exec gnaf_postgis /bin/sh -c 'chmod 600 .pgpassgeo'
 
-
 sudo docker exec gnaf_postgis /bin/sh -c 'PGPASSFILE=.pgpass psql -h localhost -U docker -d gis -c "CREATE DATABASE geo;"'
-
 sudo docker exec gnaf_postgis /bin/sh -c 'PGPASSFILE=.pgpassgeo psql -U docker -p 5432 -h localhost -d geo -c "CREATE EXTENSION IF NOT EXISTS postgis;"'
 
 sudo docker exec gnaf_postgis /bin/sh -c 'wget http://minus34.com/opendata/psma-201905/admin-bdys-201905.dmp'
 sudo docker exec gnaf_postgis /bin/sh -c 'wget http://minus34.com/opendata/psma-201905/gnaf-201905.dmp'
-
 
 sudo docker exec gnaf_postgis /bin/sh -c 'PGPASSFILE=.pgpassgeo pg_restore -Fc -d geo -p 5432 -h localhost -U docker  gnaf-201905.dmp'
 sudo docker exec gnaf_postgis /bin/sh -c 'PGPASSFILE=.pgpassgeo pg_restore -Fc -d geo -p 5432 -h localhost -U docker  admin-bdys-201905.dmp'
