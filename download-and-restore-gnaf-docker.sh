e0b40605046323b181d3d8929227dd828e7ca709
Ian <ian.moran11@gmail.com>
2019-06-30 17:11:14 +1000

    download .dmp files before creating container

--- download-and-restore-gnaf-docker.sh
+++ download-and-restore-gnaf-docker.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-sudo docker volume create gnaf_data
 
+if [ -f './admin-bdys-201905.dmp' ]; then
+   echo "./admin-bdys-201905.dmp exists."
+else
+   echo "./admin-bdys-201905.dmp does not exist. Downloading......"
+   sudo wget http://minus34.com/opendata/psma-201905/admin-bdys-201905.dmp
+
+fi
+
+
+if [ -f './gnaf-201905.dmp' ]; then
+   echo "./gnaf-201905.dmp exists."
+else
+   echo "./gnaf-201905.dmp does not exist. Downloading......"
+   sudo wget http://minus34.com/opendata/psma-201905/gnaf-201905.dmp
+fi
+
+
+sudo docker volume create gnaf_data
 sudo docker run -it -d --name=gnaf_postgis -p 5433:5432 -v gnaf_data:/var/lib/postgresql  kartoza/postgis
+
+sudo docker cp gnaf-201905.dmp gnaf_postgis:/gnaf-201905.dmp
+sudo docker cp admin-bdys-201905.dmp gnaf_postgis:/admin-bdys-201905.dmp
 
 sudo docker exec gnaf_postgis /bin/sh -c 'touch .pgpass'
 sudo docker exec gnaf_postgis /bin/sh -c 'echo "localhost:5432:gis:docker:docker" >> .pgpass'
