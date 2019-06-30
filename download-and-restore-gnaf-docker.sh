f03c5256999b13bb43e3cf8799fca62e106dce44
Ian <ian.moran11@gmail.com>
2019-06-30 18:56:04 +1000

    removing docker download script

--- download-and-restore-gnaf-docker.sh
+++ download-and-restore-gnaf-docker.sh
@@ -35,8 +35,6 @@ sudo docker exec gnaf_postgis /bin/sh -c 'chmod 600 .pgpassgeo'
 sudo docker exec gnaf_postgis /bin/sh -c 'PGPASSFILE=.pgpass psql -h localhost -U docker -d gis -c "CREATE DATABASE geo;"'
 sudo docker exec gnaf_postgis /bin/sh -c 'PGPASSFILE=.pgpassgeo psql -U docker -p 5432 -h localhost -d geo -c "CREATE EXTENSION IF NOT EXISTS postgis;"'
 
-sudo docker exec gnaf_postgis /bin/sh -c 'wget http://minus34.com/opendata/psma-201905/admin-bdys-201905.dmp'
-sudo docker exec gnaf_postgis /bin/sh -c 'wget http://minus34.com/opendata/psma-201905/gnaf-201905.dmp'
 
 sudo docker exec gnaf_postgis /bin/sh -c 'PGPASSFILE=.pgpassgeo pg_restore -Fc -d geo -p 5432 -h localhost -U docker  gnaf-201905.dmp'
 sudo docker exec gnaf_postgis /bin/sh -c 'PGPASSFILE=.pgpassgeo pg_restore -Fc -d geo -p 5432 -h localhost -U docker  admin-bdys-201905.dmp'
